fdfd466cee85f5394261c4735ae46bc8da6825b0
caryfaulknerPNNL <cary.faulkner@pnnl.gov>
2022-05-27 16:18:18 -0600

    added scripts to run reset parameter cases

---
+++ reset_chw.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+This module implements the REST API used to interact with the test case.  
+The API is implemented using the ``flask`` package.  
+
+"""
+
+# GENERAL PACKAGE IMPORT
+# ----------------------
+from testcase import EmulatorSetup
+import pandas as pd
+import time
+import csv
+import os
+import pandas as pd
+import numpy as np
+import json
+import Resets.resets as reset
+import postprocess
+
+def main(config):
+    for numiter in range(1):
+        config['name'] =str(numiter)  
+        case = EmulatorSetup(config)
+        # Set simulation step    
+        case.step = config['step']
+        measurements = case.get_measurements()
+        print(measurements)
+        with open("../run_" + config['location'] + ".config") as f:
+            config_reset = json.load(f)
+        resets = reset.setup_resets(config_reset, measurements)
+        u = {}
+        for cls in resets:
+            u[cls.control] = cls.default_setpoint
+        # Initialize u
+        # Simulation Loop
+        y = False
+        occ = 0
+        oat = 21.11
+        for i in range(int((int(config['end_time'])-int(config['start_time']))/int(config['step']))):
+            # Adjust u based on your ML method
+            if i % 10 and y and resets and numiter == 0:
+                for cls in resets:
+                    cls.update(y)
+                    r = cls.check_requests(y, i)
+                    cls.reset(r)
+                    u[cls.control] = cls.current_sp
+#                    print("Control: {}".format(u))
+            y = case.advance(u)
+
+        # print(case.get_results()['y']['PCWPum'])
+        y = pd.DataFrame.from_dict(case.get_results()['y'])
+        y.to_csv('result_{}_{}_{}.csv'.format(config['name'], numiter, config['location']))
+        u = pd.DataFrame.from_dict(case.get_results()['u'])
+        u.to_csv('input_{}_{}_{}.csv'.format(config['name'], numiter, config['location']))
+
+
+def run(location, param):
+    
+    if(param=='clg_request_thr'):
+        min_val = 0.3
+        max_val = 1
+        incr = 0.05
+        cases = 14
+        ind = 13
+    elif(param=='oat_high'):
+        min_val = 12.78
+        max_val = 24.98
+        incr = 0.2
+        cases = 61
+        ind = 5
+    elif(param=='max_sp'):
+        min_val = 6.7
+        max_val = 11.1
+        incr = 0.2
+        cases = 22
+        ind = 9
+    elif(param=='ignored_requests'):
+        min_val = 0
+        max_val = 5
+        incr = 1
+        cases = 5
+        ind = 15
+
+    for a in range(cases+1):
+        
+        val = (a/(cases))*(max_val-min_val)+min_val
+        val = str(val)
+        print('parameter, iter, val: ', param,a+1,val)
+            
+        with open('configs/'+location+'/chw_reset_default.config', 'r') as f:
+            data = f.readlines()
+            
+        data[ind] = '    "'+param+'": '+val+',\n'
+        
+        with open('configs/'+location+'/chw_reset.config', 'w') as f:
+            f.writelines(data)
+    
+            
+        fmuname = "../LargeOffice_"+location+".fmu"
+                
+        config={  
+            'fmupath':fmuname,
+            'location':location,
+            'iter':a+1,
+            'param':param,
+            'start_time':0*86400,
+            'end_time':365*86400,
+            'step': 60,
+            'default':{
+                'SupCHWTSet':6.7,
+                'SupTSetBot':12.88,
+                'SupTSetMid':12.88,
+                'SupTSetTop':12.88
+            }
+        }
+        
+        with open('run.config', 'r') as f:
+            data = f.readlines()
+        
+        data[2] = '       "dat1": "../configs/'+location+'/floor1_dat_reset_default.config",\n'
+        data[3] = '       "dat2": "../configs/'+location+'/floor2_dat_reset_default.config",\n'
+        data[4] = '       "dat3": "../configs/'+location+'/floor3_dat_reset_default.config",\n'
+        data[5] = '       "chws": "../configs/'+location+'/chw_reset.config"\n'
+
+
+        with open('run_'+location+'.config', 'w') as f:
+            f.writelines(data)
+        
+        os.mkdir('reset_'+location+'_'+param+'_'+str(a+1))
+        os.chdir('reset_'+location+'_'+param+'_'+str(a+1))
+        main(config)
+        postprocess.plot_main(location)
+        os.chdir('../')
